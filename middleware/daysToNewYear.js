38121e300785cc6efcb12d0b53457918cb14b39b
Raquel Marie <raqueldelamer@gmail.com>
2024-12-09 00:05:22 -0600

    update daysToNewYear

--- middleware/daysToNewYear.js
+++ middleware/daysToNewYear.js
@@ -2,7 +2,7 @@ const daysToNewYear = (req, res, next) => {
     const dateToday = new Date();
     const newYear = new Date('January 1, 2025 00:00:00');
     const millisecondsToNewYear = newYear - dateToday;
-    console.log("Days to New Year 2025: " + millisecondsToNewYear / 60 / 60/ 24 / 1000);
+    console.log("Days until New Year 2025: " + millisecondsToNewYear / 60 / 60/ 24 / 1000 + " Woohoo!! 2025!!");
     req.daysToNewYear = (millisecondsToNewYear / 60 / 60 / 24 / 1000);
     next();
 }
